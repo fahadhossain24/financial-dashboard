061b0d458b6c4a7519f7d8f6c5525e4080659d32
fahadhossain24 <fahadhossaim24@gmail.com>
2023-11-03 23:11:40 +0600

    complete 2nd chapter - styling next project

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h2>welcome my first next app. </h2>
-    </div>
+    <main className='flex min-h-screen flex-col p-6'>
+      <div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-600 p-4 md:h-52'>
+        <div
+          className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent "
+        />
+      </div>
+    </main>
   )
 }
