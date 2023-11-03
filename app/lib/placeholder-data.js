1b6e3cf3ff8870652a27bccac839a2c86682bbf1
fahadhossain24 <fahadhossaim24@gmail.com>
2023-11-03 22:31:56 +0600

    initial commit project setup

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,20 @@
+const invoice = [
+    {
+        customeId: customers[0].id,
+        amount: 15723,
+        status: 'pending',
+        date: '2023-12-14',
+    },
+    {
+        customer_id: customers[0].id,
+        amount: 15723,
+        status: 'pending',
+        date: '2023-12-14',
+    },
+    {
+        customer_id: customers[0].id,
+        amount: 15723,
+        status: 'pending',
+        date: '2023-12-14',
+    },
+]
