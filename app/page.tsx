125cbef8acc90bd2cbd6807b565ee29b44e56da7
fahadhossain24 <fahadhossaim24@gmail.com>
2023-11-05 23:25:03 +0600

    next style

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col p-6'>
       <div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-600 p-4 md:h-52'>
-        <div
-          className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent "
-        />
+        <h2>next fronts and image</h2>
       </div>
     </main>
   )
