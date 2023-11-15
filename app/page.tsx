d457d9b91c0f3e1a4e11c772d2dceda44ee011e5
Fahad Hossain <105900280+fahadhossain24@users.noreply.github.com>
2023-11-15 19:52:16 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col p-6'>
       <div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-600 p-4 md:h-52'>
-        <h2>next fronts and image</h2>
+        <h2>next fronts</h2>
       </div>
     </main>
   )
